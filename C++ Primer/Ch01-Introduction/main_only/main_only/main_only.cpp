3efa3e257a7d3eeaae44da7d46db29224ca75c25
Sevenshards <sevenshards@hanyang.ac.kr>
2023-12-30 14:00:12 +0900

    C++ 맛보기 - main 함수만 만들기

---
+++ C++ Primer/Ch01-Introduction/main_only/main_only/main_only.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* C++ Primer - Introduction (서론)
+* 파일명: main_only.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-30
+* 이전 버전 작성 일자:
+* 버전 내용: C++ 맛보기 - main 함수만 만들기
+* 이전 버전 내용:
+*/
+
+int main()
+{
+	return 0;
+}
