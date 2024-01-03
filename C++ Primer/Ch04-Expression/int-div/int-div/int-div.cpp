f67cce9f15a47213259ad38b6f6f0008f1b14ba5
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-03 17:25:23 +0900

    간단한 정수형 나눗셈 예제

---
+++ C++ Primer/Ch04-Expression/int-div/int-div/int-div.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* C++ Primer - Expression (표현식)
+* 파일명: int-div.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-03
+* 이전 버전 작성 일자:
+* 버전 내용: 간단한 정수형 나눗셈 예제
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(int argc, const char* argv[])
+{
+	int ival1 = 21 / 6;
+
+	int ival2 = 21 / 7;
+
+	cout << ival1 << " " << ival2 << endl;
+
+	return 0;
+}
