8f0cd9a9b08cf53f01d6763feb86d319485d4fee
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-03 20:25:29 +0900

    sizeof 연산자 사용 예제

---
+++ C++ Primer/Ch04-Expression/sizeof_pgm/sizeof_pgm/Sales_data.h
@@ -0,0 +1,32 @@
+﻿/*
+* C++ Primer - Variable & Types (변수와 기본 타입)
+* 파일명: Sales_data.h
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2023-12-31
+* 이전 버전 작성 일자:
+* 버전 내용: Sales_data 클래스 정의
+* 이전 버전 내용:
+*/
+
+#ifndef __SALES_DATA_H__
+#define __SALES_DATA_H__
+
+#include <iostream>
+#include <string> // string 클래스를 사용하기 위함
+using std::string;
+
+// struct는 구조체지만 C++에서는 클래스로 볼 수 있다.
+// 일단 클래스에 대한 개념을 정립하기 전까지는 struct로
+struct Sales_data
+{
+	string bookNo;
+#ifdef IN_CLASS_INITS // 조건부 컴파일 시작
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+#else
+	unsigned units_sold;
+	double revenue;
+#endif // 조건부 컴파일 종료
+};
+#endif
---
+++ C++ Primer/Ch04-Expression/sizeof_pgm/sizeof_pgm/sizeof_pgm.cpp
@@ -0,0 +1,65 @@
+﻿/*
+* C++ Primer - Expression (표현식)
+* 파일명: sizeof_pgm.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-03
+* 이전 버전 작성 일자:
+* 버전 내용: sizeof 연산자 사용 예제
+* 이전 버전 내용:
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "Sales_data.h"
+
+#define SIZEOF_MEMBER
+
+int main(int argc, const char* argv[])
+{
+	Sales_data data, * p;
+	sizeof(Sales_data);
+	sizeof data;
+	sizeof p;
+	sizeof* p;
+	sizeof data.revenue;
+
+#ifdef SIZEOF_MEMBER // C++11 표준으로 추가된 부분. 범위 접근 연산자(::)를 사용하여 타입 멤버 크기를 얻는 것이 가능
+	sizeof Sales_data::revenue;
+#else
+	sizeof Sales_data().revenue; // 임시객체를 생성해서 해당 멤버에 접근하여 sizeof 연산을 수행
+#endif
+
+	cout << "short: " << sizeof(short) << '\n'
+		<< "short[3]: " << sizeof(short[3]) << '\n'
+		<< "short*: " << sizeof(short*) << '\n'
+		<< "short&: " << sizeof(short&) << endl;
+
+	cout << "int: " << sizeof(int) << '\n'
+		<< "int[3]: " << sizeof(int[3]) << '\n'
+		<< "int*: " << sizeof(int*) << '\n'
+		<< "int&: " << sizeof(int&) << endl;
+
+	cout << "Sales_data: " << sizeof(Sales_data) << '\n'
+		<< "Sales_data[3]: " << sizeof(Sales_data[3]) << '\n'
+		<< "Sales_data*: " << sizeof(Sales_data*) << '\n'
+		<< "Sales_data&: " << sizeof(Sales_data&) << endl;
+
+#ifdef SIZEOF_MEMBER // C++11 표준으로 추가된 부분. 범위 접근 연산자(::)를 사용하여 타입 멤버 크기를 얻는 것이 가능
+	cout << "Sales_data::revenue: " << sizeof Sales_data::revenue << '\n'
+		<< "data.revenue: " << sizeof data.revenue << endl;
+#else
+	cout << "Sales_data().revenue: " << sizeof Sales_data().revenue << '\n' // 임시객체를 생성해서 해당 멤버에 접근하여 sizeof 연산을 수행
+		<< "data.revenue: " << sizeof data.revenue << endl;
+#endif
+
+	int x[10];
+	int* ip = x;
+
+	cout << sizeof(x) / sizeof(*x) << endl; // 배열 요소 개수
+	cout << sizeof(ip) / sizeof(*ip) << endl; // int형 포인터의 크기를 int형 크기로 나눔
+
+	return 0;
+}
