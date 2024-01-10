9900d1103c550af9899bb31ea051fb4318119aa0
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-10 16:32:00 +0900

    순서 없는 연관 컨테이너 기본 예제 (C++11 표준 이상)

---
+++ C++ Primer/Ch11-AssociativeContainer/unorderedWordCount/unorderedWordCount/Sales_data.h
@@ -0,0 +1,90 @@
+﻿/*
+* C++ Primer - Class (클래스)
+* 파일명: Sales_data.h
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-06
+* 이전 버전 작성 일자:
+* 버전 내용: 클래스 기본 예제 - 헤더 파일
+* 이전 버전 내용:
+*/
+
+#ifndef __SALES_DATA_H__
+#define __SALES_DATA_H__
+
+#define IN_CLASS_INITS // 클래스 내 초기화를 사용할 경우
+#define DEFAULT_FCNS // C++11 표준 이후로 지원하는 컴파일러가 생성하는 기본 생성자를 사용할 경우
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+class Sales_data
+{
+private:
+	string bookNo;
+#ifdef IN_CLASS_INITS // 클래스 내 초기화를 사용할 경우
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+#else
+	unsigned units_sold;
+	double revenue = 0.0;
+#endif
+public:
+	// 생성자 정의
+	// 기본 생성자
+	// 클래스 본체 내부 또는 외부에서 정의 가능
+	// 클래스 내에서 정의하면 기본적으로 inline화
+#if defined(IN_CLASS_INITS) && defined(DEFAULT_FCNS) // 클래스 내 초기화 + C++11 표준 컴파일러가 생성하는 기본 생성자를 사용할 경우
+	Sales_data() = default; // 기본 생성자 사용 (C++11 이후 표준)
+#else // 클래스 내 초기화를 사용하지 않을 경우 -> 멤버 이니셜라이저 사용
+	Sales_data() : units_sold(0), revenue(0.0) { }
+#endif
+	// string을 인자로 받는 생성자
+#ifdef IN_CLASS_INITS // 클래스 내 초기화를 사용할 경우
+	Sales_data(const string &s) : bookNo(s) { }
+#else // 클래스 내 초기화를 사용하지 않을 경우 -> 멤버 이니셜라이저 사용
+	Sales_data(const string &s) : bookNo(s), units_sold(0), revenue(0.0) { }
+#endif
+	// 인자를 3개를 받는 생성자
+	Sales_data(const string &s, unsigned n, double p)
+		:bookNo(s), units_sold(n), revenue(p *n) { }
+
+	// Sales_data 객체에 대한 함수들
+	// 멤버 함수
+	// 클래스 본체 내에서 멤버 함수를 정의
+	string isbn() const { return this->bookNo; } // const 멤버 함수 -> 값을 바꿀 일이 없으며 조회만을 목적으로 한다.
+	// 아래의 두 멤버 함수는 클래스 외부에서 정의
+	Sales_data &combine(const Sales_data &);
+	double avg_price() const;
+
+	// 비멤버 함수
+	// friend 선언을 통해서 Sales_data 객체의 private 데이터 멤버에 접근 가능
+	friend Sales_data add(const Sales_data &, const Sales_data &);
+	friend ostream &print(ostream &, const Sales_data &);
+	friend istream &read(istream &, Sales_data &);
+
+};
+
+// Sales_data에 대한 비멤버 함수 선언 -> interface
+Sales_data add(const Sales_data &, const Sales_data &);
+ostream &print(ostream &, const Sales_data &);
+istream &read(istream &, Sales_data &);
+
+// multimap에서 사용될 비교 연산 함수
+// 순서 있는 연관 컨테이너에서는 key 타입에 대해 순약순서(strict weak ordering)이 정의되어 있어야 한다.
+// 순약순서에 대해서 간단하게 정의하면 '보다 작다'의 개념이 있어야 한다.
+// 순약순서를 정의하는 비교 함수에서는 다음의 특성을 지녀야 한다.
+// 1) 두 key가 모두 상대보다 작을 수 없다 -> k1 < k2 라면 반드시 k2 > k1이 성립한다. k2 < k1이 성립될 수 없다.
+// 2) k1 < k2, k2 < k3라면 반드시 k1 < k3가 성립한다.
+// 3) k1, k2가 서로 작지 않다라면 k1 == k2, 상등 관계가 성립해야 한다.
+// 그리고 k1 == k2, k2 == k3라면 k1 == k3 역시 성립해야 한다.
+inline
+bool CompareIsbn(const Sales_data &lhs, const Sales_data &rhs)
+{
+	return lhs.isbn() < rhs.isbn();
+}
+#endif
---
+++ C++ Primer/Ch11-AssociativeContainer/unorderedWordCount/unorderedWordCount/unorderedWordCount.cpp
@@ -0,0 +1,97 @@
+﻿/*
+* C++ Primer - Associative Container (연관 컨테이너)
+* 파일명: unorderedWordCount.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-01-10
+* 이전 버전 작성 일자:
+* 버전 내용: 순서 없는 연관 컨테이너 기본 예제 (C++11 표준 이상)
+* 이전 버전 내용:
+*/
+
+#include <unordered_map>
+using std::unordered_map;
+
+#include <unordered_set>
+using std::unordered_set;
+using std::unordered_multiset;
+
+#include <string>
+using std::string;
+
+using std::hash;
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#include <cstddef>
+using std::size_t;
+
+#include "Sales_data.h"
+
+#define TYPE_ALIAS_DECLS // using을 이용한 타입 별칭을 사용하는 경우 (C++11 표준 이상)
+
+// map이 아닌 unordered_map, 순서 없는 map 기준의 word_count
+int main(int argc, const char* argv[])
+{
+	// 단어가 발생한 횟수를 세지만, map과 달리 사전 편찬 순서가 아니다.
+	unordered_map<string, size_t> word_count;
+	string word;
+	while (cin >> word)
+		++word_count[word];
+
+	for (const auto &w : word_count)
+		cout << w.first << " occurs " << w.second << ((w.second > 1) ? " times" : " time") << endl;
+
+	return 0;
+}
+
+// 순서 없는 연관 컨테이너에서도 Key의 type에 조건이 있다.
+// multimap의 경우에는 Key의 type이 < 연산이 가능한 순약순서가 정의되어 있어야 했다.
+// 여기서도 마찬가지로 다음의 조건이 필요하다.
+// 
+// 1) Key는 상등 연산(==)이 가능해야 한다.
+// 2) 해시 코드를 계산할 수 있어야 한다.
+// 
+// multimap에서 CompareIsbn을 사용했던 것과 마찬가지로 Sales_data를 키로 사용하기 위해서는 두 가지가 필요하다
+// 1) 상등 연산자를 대체할 함수
+// 2) 해시 코드를 계산하는 함수
+// 
+// 아래에는 Sales_data를 Key type으로 사용하기 위해 기본 hash와 상등 연산자로 사용할 함수를 재정의(override)한다.
+size_t hasher(const Sales_data &sd)
+{
+	return hash<string>()(sd.isbn());
+}
+bool eqOp(const Sales_data &lhs, const Sales_data &rhs)
+{
+	return lhs.isbn() == rhs.isbn();
+}
+
+// 타입 별칭을 사용하여 타입을 간소화
+// 전달하는 인자로 위에서 정의한 함수를 포인터로 전달
+#ifdef TYPE_ALIAS_DECLS // using을 이용한 타입 별칭을 사용하는 경우 (C++11 표준 이상)
+using SD_multiset = unordered_multiset<Sales_data, decltype(hasher) *, decltype(eqOp) *>;
+#else // using을 이용한 타입 별칭을 사용하지 않는 경우 (C++11 표준 미만)
+typedef unordered_multiset<Sales_data, decltype(hasher) *, decltype(eqOp) *> SD_multiset;
+#endif
+
+// bookstore 객체는 같은 ISBN에 대해서 여러 Sales_data 객체를 가질 수 있음
+// 전달되는 인자는 버킷의 크기, 해시 함수에 대한 포인터와 상등 연산자 함수에 대한 포인터다.
+SD_multiset bookstore(42, hasher, eqOp);
+
+// 해시 함수를 재정의 하는 방법
+// 먼저, Foo 객체는 반드시 상등 연산(==)이 가능해야 한다.
+struct Foo { string s; };
+
+// 연산자 오버로딩을 이용하여 Foo 객체의 상등 연산(==)을 정의.
+// 정확한 내용은 14장에서 다룬다.
+bool operator==(const Foo &l, const Foo &r) { return l.s == r.s; }
+
+// FooHash를 이용하여 해시 함수를 재정의(override)
+size_t FooHash(const Foo &f) { return hash<string>()(f.s); }
+
+// FooHash를 이용하여 해시 코드를 생성한다.
+// Foo에는 상등 연산자가 정의되어 있으므로 인자로 전달할 필요는 없음.
+unordered_set<Foo, decltype(FooHash) *> fooSet(10, FooHash);
