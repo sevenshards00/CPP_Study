1c4b4504b6306b1841afdd444899efe6b6987b48
Sevenshards <sevenshards@hanyang.ac.kr>
2024-01-12 15:26:38 +0900

    using 선언 제거

--- C++ Primer/Ch12-DynamicMemory/allocSP/allocSP/Foo.h
+++ C++ Primer/Ch12-DynamicMemory/allocSP/allocSP/Foo.h
@@ -1,19 +1,18 @@
 ﻿/*
 * C++ Primer - Dynamic Memory & Smart Pointer (동적 메모리와 스마트 포인터)
 * 파일명: Foo.h
-* 파일 버전: 0.1
+* 파일 버전: 0.2
 * 작성자: Sevenshards
-* 작성 일자: 2024-01-11
-* 이전 버전 작성 일자:
-* 버전 내용: shared_ptr 기본 사용 예제
-* 이전 버전 내용:
+* 작성 일자: 2024-01-12
+* 이전 버전 작성 일자: 2024-01-11
+* 버전 내용: using 선언 제거
+* 이전 버전 내용: shared_ptr 기본 사용 예제
 */
 
 #ifndef __FOO_H__
 #define __FOO_H__
 
 #include <iostream>
-using std::ostream;
 
 using T = int;
 // struct == 멤버가 모두 기본 public인 class
@@ -23,7 +22,7 @@ struct Foo
 	T val;
 };
 
-ostream &print(ostream &os, const Foo &f)
+std::ostream &print(std::ostream &os, const Foo &f)
 {
 	os << f.val;
 	return os;
