4e3b0230f16fc02178609c6f9a27ff94dec07043
Sevenshards <sevenshards@hanyang.ac.kr>
2024-02-20 13:11:24 +0900

    클래스의 선언과 정의 예제

---
+++ Fundamental C++/Ch02-Declaration&Definition/ClassDeclarationDefinitionSample/ClassDeclarationDefinitionSample/A.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* Fundamental C++ - Ch02-Declaration&Definition
+* 파일명: A.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-02-20
+* 이전 버전 작성 일자:
+* 버전 내용: 클래스의 선언과 정의 예제
+* 이전 버전 내용:
+*/
+
+int g_Val;		// 전역 객체 선언 및 정의
+class CTest		// 클래스 선언 및 정의
+{
+public:
+	int m_Val;
+};
