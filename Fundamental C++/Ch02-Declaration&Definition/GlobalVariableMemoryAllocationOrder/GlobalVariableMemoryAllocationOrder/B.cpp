09549278b7ddfcd1da265b8e018f7183eb6d2e51
Sevenshards <sevenshards@hanyang.ac.kr>
2024-02-20 16:50:27 +0900

    전역 변수가 초기화되는 순서 예제

---
+++ Fundamental C++/Ch02-Declaration&Definition/GlobalVariableMemoryAllocationOrder/GlobalVariableMemoryAllocationOrder/B.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Fundamental C++ - Ch02-Declaration&Definition
+* 파일명: B.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-02-20
+* 이전 버전 작성 일자:
+* 버전 내용: 전역 변수가 초기화되는 순서 예제
+* 이전 버전 내용:
+*/
+
+int Func(int arg);
+
+int g_Val5 = Func(5);
+int g_Val6 = 6;
