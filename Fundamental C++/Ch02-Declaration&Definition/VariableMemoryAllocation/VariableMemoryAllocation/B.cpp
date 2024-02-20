192ba9b20fbbb4af7c6e6d9f87099600a120aa0a
Sevenshards <sevenshards@hanyang.ac.kr>
2024-02-20 16:17:23 +0900

    변수가 메모리에 할당되는 순서

---
+++ Fundamental C++/Ch02-Declaration&Definition/VariableMemoryAllocation/VariableMemoryAllocation/B.cpp
@@ -0,0 +1,14 @@
+﻿/*
+* Fundamental C++ - Ch02-Declaration&Definition
+* 파일명: B.cpp
+* 파일 버전: 0.1
+* 작성자: Sevenshards
+* 작성 일자: 2024-02-20
+* 이전 버전 작성 일자:
+* 버전 내용: 변수가 메모리에 할당되는 순서
+* 이전 버전 내용:
+*/
+
+int g_ValB1;
+int g_ValB2;
+static int s_Val;
